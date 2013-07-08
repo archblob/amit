1aa8eed5a1427e7f9d688ad0a27e53d3465986b6
archblob <fcsernik@gmail.com>
2013-07-08 20:45:33 +0300

    Add simple view.

--- src/js/views.js
+++ src/js/views.js
@@ -19,6 +19,12 @@
 
     this.ctx  = this.cvs.getContext('2d');
 
+    this.peek = { note : { name : "Init",
+      frequency : 0.00},
+      cents : 0,
+      frequency : 0.00
+    };
+
     this.color         = "rgb(58,58,58)";
     this.noteFont      = "50px sans-serif";
     this.freqFont      = "20px sans-serif";
@@ -30,12 +36,6 @@
 
     this.cvs.width  = 400;
     this.cvs.height = 200;
-
-    this.peek = { note : { name : "Init",
-                           frequency : 0.00},
-                  cents : 0,
-                  frequency : 0.00
-     };
 
     this.centerX       = this.cvs.width / 2;
     this.centerY       = this.cvs.height;
@@ -110,8 +110,85 @@
   CentsView.prototype.update = function (element) {
 
     this.peek = element.peek;
+
   };
 
-  global['CentsView'] = CentsView;
+  function SimpleView() {
+
+    ViewContextAndStyle.apply(this, arguments);
+
+    this.cvs.width  = 200;
+    this.cvs.height = 100;
+    
+    this.xpad = 10;
+    this.ypad = 10;
+    this.maj  = 30;
+    this.min  = 30;
+    this.cx = this.cvs.width / 2;
+    this.cy = this.cvs.height / 2;
+  }
+
+  SimpleView.prototype.drawArrow = function (x, y, semiMajor, semiMinor, direction) {
+
+    var dir = semiMinor * direction;
+
+    this.ctx.beginPath();
+
+    this.ctx.moveTo(x - semiMajor,y);
+    this.ctx.lineTo(x,y + dir);
+    this.ctx.lineTo(x + semiMajor, y);
+    this.ctx.bezierCurveTo(x, y + (dir / 2), x, y + (dir / 2), x - semiMajor, y);
+
+    this.ctx.fill();
+
+  };
+
+  SimpleView.prototype.drawNoteName = function (name) {
+
+    this.ctx.textAlign    = 'left';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.font         = "60pt Arial";
+
+    this.ctx.fillText(name,this.xpad,this.cy);
+
+  };
+
+  SimpleView.prototype.drawFrequency = function (x, y,freq) {
+
+    this.ctx.textAlign    = 'center';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.font         = "10pt Arial";
+
+    this.ctx.fillText(freq.toString(),x,y);
+
+  };
+
+  SimpleView.prototype.run = function () {
+
+    var noteName  = this.peek.note.name;
+    var frequency = this.peek.frequency.toFixed(2) + ' Hz';
+    var cents     = this.peek.cents;
+    var arrowx    = this.cvs.width - this.maj - this.xpad;
+
+    this.ctx.clearRect(0, 0, this.cvs.width, this.cvs.height);
+
+    var tuneDir = cents > 0 ? 1 : -1;
+
+    this.drawArrow(arrowx,this.cy + (10 * tuneDir),this.maj,this.min,tuneDir);
+
+    this.drawFrequency(arrowx, this.cy,frequency);
+    this.drawNoteName(noteName);
+    window.requestAnimationFrame(this.run.bind(this));
+
+  };
+
+  SimpleView.prototype.update = function (element) {
+
+    this.peek = element.peek;
+
+  };
+
+  global['CentsView']  = CentsView;
+  global['SimpleView'] = SimpleView;
 
 }(window));
