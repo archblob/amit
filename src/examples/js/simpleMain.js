9993c0286bf6a54d35614653a6c7cbb544f2656e
archblob <fcsernik@gmail.com>
2013-07-09 21:34:29 +0300

    Add simpleView example files.

---
+++ src/examples/js/simpleMain.js
@@ -0,0 +1,25 @@
+window.AudioContext =
+  window.AudioContext || window.webkitAudioContext ||
+  window.mozAudioContext || window.msAudioContext;
+
+navigator.getUserMedia =
+  navigator.getUserMedia || navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+if (window.AudioContext && navigator.getUserMedia){
+
+  navigator.getUserMedia({audio : true},
+    function (stream) {
+
+      var view     = new SimpleView("viewContainer");
+      var tuner    = new Tuner(view.update.bind(view));
+
+      view.run();
+      tuner.run(stream);
+
+  }, function (e) {
+    console.log(e);
+  });
+} else {
+  alert("Web Audio not supported, please enable it or use another browser.");
+}
