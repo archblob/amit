6f4bc9edbbba7e0d6a73864e63d1546d3d8131b9
archblob <fcsernik@gmail.com>
2013-09-02 18:43:36 +0300

    Adjust formatting and remove dead code.

--- src/views/common.js
+++ src/views/common.js
@@ -6,24 +6,16 @@
  */
 
 window.requestAnimationFrame =
-    window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-/* default color scheme */
-var green = "rgb(122,153,66)";
-var black = "rgb(58,58,58)";
-var lightBlack = "rgba(58,58,58, 0.2)";
-var white = "rgb(227,227,227)";
-var red   = "rgb(140,46,46)";
-var blue  = "rgb(44,114,158)";
+  window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
 var defaultPeek = {
-      note : {
-        name : "I",
-        frequency : 0.00
-      },
-      cents : 0,
-      frequency : 0.00
+  note : {
+    name : "I",
+    frequency : 0.00
+  },
+  cents : 0,
+  frequency : 0.00
 };
 
 function fontStringPX(size, name) {
