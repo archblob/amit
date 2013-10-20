157ef67d98b4a7f8aebf4bbe1f3a24f426cab3b3
archblob <fcsernik@gmail.com>
2013-10-20 03:48:43 +0300

    Lose the underscores in Tuner definition.

--- src/tuner/tuner.js
+++ src/tuner/tuner.js
@@ -6,22 +6,21 @@ function Tuner() {
 
   Tuner.prototype._instance = this;
 
-  var context              = new AudioContext()
-    , _samplerate          = 44100
-    , _downsampleFactor    = 12
-    , _fftSize             = 2048
-    , _effectiveSamplerate = _samplerate / _downsampleFactor
-    , _frequencyResolution = _effectiveSamplerate / _fftSize
-    , _bufferSize          = _fftSize * _downsampleFactor
-    /* measured in seconds here */
-    , _temporalWindow      = _bufferSize / _samplerate
-    , _harmonics           = 5
-    , _maxHarmFrequency    = _fftSize / _harmonics * _frequencyResolution
-    , fft                  = new FFT(_fftSize, _effectiveSamplerate)
-    , samples              = new Ring(_bufferSize, 512)
-    , _windowFunction      = new WindowObject("Hann", _bufferSize)
-    , _frequencyMap        = new FrequencyMap()
-    , _viewCallback
+  var context             = new AudioContext()
+    , samplerate          = 44100
+    , downsampleFactor    = 12
+    , fftSize             = 2048
+    , effectiveSamplerate = samplerate / downsampleFactor
+    , frequencyResolution = effectiveSamplerate / fftSize
+    , bufferSize          = fftSize * downsampleFactor
+    , temporalWindow      = bufferSize / samplerate /* seconds */
+    , harmonics           = 5
+    , maxHarmFrequency    = fftSize / harmonics * frequencyResolution
+    , fft                 = new FFT(fftSize, effectiveSamplerate)
+    , samples             = new Ring(bufferSize, 512)
+    , windowFunction      = new WindowObject("Hann", bufferSize)
+    , frequencyMap        = new FrequencyMap()
+    , viewCallback
     , requestDataType = { peek       : false
                         , spectrum   : false
                         , updateTime : false
@@ -43,26 +42,26 @@ function Tuner() {
       , respondData = {}
       ;
 
-    _windowFunction.process(samples);
+    windowFunction.process(samples);
 
-    for (i = 0; i < samples.length ; i += _downsampleFactor) {
+    for (i = 0; i < samples.length ; i += downsampleFactor) {
       downsampled.push(samples.get(i));
     }
 
     fft.forward(downsampled);
 
     spectrum = fft.spectrum;
-    peek     = HPS(spectrum,_harmonics);
+    peek     = HPS(spectrum,harmonics);
 
     if (requestDataType.peek) {
-      respondData.peek = _frequencyMap.closestNote(peek * _frequencyResolution);
+      respondData.peek = frequencyMap.closestNote(peek * frequencyResolution);
     }
 
     if (requestDataType.spectrum) {
       respondData.spectrum = spectrum;
     }
 
-    _viewCallback(respondData);
+    viewCallback(respondData);
   }
 
   Object.defineProperties(this, {
@@ -77,12 +76,12 @@ function Tuner() {
 
             lowpass.type       = "lowpass";
             highpass.type      = "highpass";
-            lowpass.frequency  = (_effectiveSamplerate / 2).toFixed(3);
+            lowpass.frequency  = (effectiveSamplerate / 2).toFixed(3);
             highpass.frequency = 35;
         }
       },
       "samplerate" : {
-          value        : _samplerate
+          value        : samplerate
         , enumerable   : true
         , configurable : false
         , writable     : false
@@ -91,10 +90,10 @@ function Tuner() {
           enumerable   : false
         , configurable : false
         , set : function (clbk) {
-          _viewCallback = clbk;
+          viewCallback = clbk;
 
           if (requestDataType.updateTime) {
-            _viewCallback({ updateTime : _temporalWindow });
+            viewCallback({ updateTime : temporalWindow });
           }
         }
       }
@@ -112,29 +111,29 @@ function Tuner() {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _downsampleFactor;
+            return downsampleFactor;
           }
         , set : function (value) {
 
             checkNat("downsampleFactor",value);
 
-            _downsampleFactor = Math.floor(value);
+            downsampleFactor = Math.floor(value);
 
-            _effectiveSamplerate = _samplerate / _downsampleFactor;
-            _frequencyResolution = _effectiveSamplerate / _fftSize;
-            _bufferSize       = _fftSize * _downsampleFactor;
-            _temporalWindow   = _bufferSize / _samplerate;
-            _maxHarmFrequency = _fftSize / _harmonics * _frequencyResolution;
+            effectiveSamplerate = samplerate / downsampleFactor;
+            frequencyResolution = effectiveSamplerate / fftSize;
+            bufferSize       = fftSize * downsampleFactor;
+            temporalWindow   = bufferSize / samplerate;
+            maxHarmFrequency = fftSize / harmonics * frequencyResolution;
 
-            fft     = new FFT(_fftSize, _effectiveSamplerate);
-            samples = new Ring(_bufferSize, 512);
+            fft     = new FFT(fftSize, effectiveSamplerate);
+            samples = new Ring(bufferSize, 512);
 
-            lowpass.frequency  = (_effectiveSamplerate / 2).toFixed(3);
+            lowpass.frequency  = (effectiveSamplerate / 2).toFixed(3);
 
-            _windowFunction.length = _bufferSize;
+            windowFunction.length = bufferSize;
 
             if (requestDataType.updateTime) {
-              _viewCallback({ updateTime : _temporalWindow });
+              viewCallback({ updateTime : temporalWindow });
             }
           }
       }
@@ -142,24 +141,24 @@ function Tuner() {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _fftSize;
+            return fftSize;
           }
         , set : function (value) {
-            _fftSize = value;
+            fftSize = value;
 
-            _frequencyResolution = _effectiveSamplerate / _fftSize;
-            _bufferSize       = _fftSize * _downsampleFactor;
-            _temporalWindow   = _bufferSize / _samplerate;
-            _maxHarmFrequency = _fftSize / _harmonics * _frequencyResolution;
+            frequencyResolution = effectiveSamplerate / fftSize;
+            bufferSize       = fftSize * downsampleFactor;
+            temporalWindow   = bufferSize / samplerate;
+            maxHarmFrequency = fftSize / harmonics * frequencyResolution;
 
             /* this will throw an exception if the fft size is not valid */
-            fft     = new FFT(_fftSize, _effectiveSamplerate);
-            samples = new Ring(_bufferSize, 512);
+            fft     = new FFT(fftSize, effectiveSamplerate);
+            samples = new Ring(bufferSize, 512);
 
-            _windowFunction.length = _bufferSize;
+            windowFunction.length = bufferSize;
 
             if (requestDataType.updateTime) {
-              _viewCallback({ updateTime : _temporalWindow });
+              viewCallback({ updateTime : temporalWindow });
             }
           }
       }
@@ -167,33 +166,33 @@ function Tuner() {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _effectiveSamplerate;
+            return effectiveSamplerate;
           }
       }
     , "frequencyResolution" : {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _frequencyResolution;
+            return frequencyResolution;
           }
       }
     , "temporalWindow" : {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _temporalWindow;
+            return temporalWindow;
           }
       }
     , "frequencyMap" : {
           enumerable   : false
         , configurable : false
         , get : function () {
-            return _frequencyMap;
+            return frequencyMap;
           }
         , set : function (freqMap) {
 
             if (freqMap.hasOwnProperty("closestNote")) {
-              _frequencyMap   = freqMap;
+              frequencyMap   = freqMap;
             } else {
               throw new TypeError("Passed object has to " +
                                   "implement method closestNote");
@@ -204,14 +203,14 @@ function Tuner() {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _harmonics;
+            return harmonics;
           }
         , set : function (value) {
 
             checkNat("downsampleFactor",value);
 
-            _harmonics        = value;
-            _maxHarmFrequency = _fftSize / _harmonics * _frequencyResolution;
+            harmonics        = value;
+            maxHarmFrequency = fftSize / harmonics * frequencyResolution;
 
           }
       }
@@ -219,7 +218,7 @@ function Tuner() {
           enumerable   : true
         , configurable : false
         , get : function () {
-            return _maxHarmFrequency;
+            return maxHarmFrequency;
         }
     }
     , "run" : {
@@ -229,7 +228,7 @@ function Tuner() {
               throw new ReferenceError("The audio stream is not set.");
             }
 
-            if (!_viewCallback) {
+            if (!viewCallback) {
               throw new PropertyNotInitialized("Tuner", "callback");
             }
 
@@ -247,7 +246,7 @@ function Tuner() {
             processor.connect(context.destination);
 
             return window.setInterval(fundamental
-                                     , _temporalWindow.toFixed(3) * 1000);
+                                     , temporalWindow.toFixed(3) * 1000);
         }
         , enumerable   : false
         , configurable : false
