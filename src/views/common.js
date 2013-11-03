9cb35922ec70584117019da980d87cfbab89a3df
archblob <fcsernik@gmail.com>
2013-11-03 19:15:06 +0200

    Add TunerView.

--- src/views/common.js
+++ src/views/common.js
@@ -23,3 +23,55 @@ var defaultPeek = {
 function fontStringPX(size, name) {
   return size + "px " + name;
 }
+
+function TunerView(containerID) {
+  var canvases = []
+    , reqCvs   = 0
+    , viewType
+    , viewContainer = document.getElementById(containerID)
+    , viewWrapper   = document.createElement("div")
+    ;
+    
+  viewWrapper.id = "tunerWrapper"
+  /* TODO: check to see if the container is found. */
+
+  Object.defineProperties(this, {
+    "viewType" : {
+        enumerable   : true
+      , configurable : false
+      , get : function () {
+          return viewType;
+        }
+      , set : function (vT) {
+          /* TODO: checks if it is a function and
+           * and methods has the necessary methods.
+           */
+          viewType = vT;
+          /* TODO: check to see it it's a natural number */
+          reqCvs   = viewType.requiredCVS;
+          var depth = canvases.length;
+
+          while (depth != reqCvs) {
+            if (depth < reqCvs) {
+              canvases[depth] = document.createElement("canvas");
+              depth += 1;
+            } else {
+              viewWrapper.removeChild(canvases[depth]);
+              canvases.pop();
+              depth -= 1;
+            }
+          }
+
+          for (cvs in canvases) {
+            viewWrapper.appendChild(canvases[cvs]);
+          }
+
+          viewContainer.appendChild(viewWrapper);
+
+          viewType.setCVS(canvases);
+          viewType.start();
+        }
+    }
+  })
+
+}
